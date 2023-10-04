70619efd9abca7ee245b81ea50875d2624caac68
Ricardo Barone <ricardobarone@outlook.com>
2023-10-04 20:30:56 -0300

    se agregó manejo de errores

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -49,7 +49,14 @@ export const updateProduct = async (req, res) => {
     let { pid } = req.params;
     let { title, description, price, stock } = req.body;
     let prod;
-    if (!title || !description || !price || !stock) return res.send({ status: "error", error: "Falta completar valores" });
+    if (!title || !description || !price || !stock){
+        CustomError.createError({
+            name: "Product update error",
+            cause: generateProductErrorInfo({title, description, price, stock}),
+            message: "Error trying to create Product",
+            code: EErrors.INVALID_TYPES_ERROR
+        })
+    }
     try {
         prod = await prodService.updProducts(pid, { title, description, price, stock })
     } catch (error) {
